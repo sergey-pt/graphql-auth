a976ef6ece9052ad3f31e40ebb01e494d2e5942f
Sergei Anisimov <pearce89@gmail.com>
2019-08-04 13:40:43 +0300

    feat (Styles): add some responsive rules

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container p-0 xl:p-4">
     <Header />
     <div class="container mx-auto sm:block md:block lg:flex xl:flex py-4">
       <nuxt />
